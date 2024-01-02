a904ef1b649aa9b880a9f2e59806d00290840bd7
lapeko <lapeko88@gmail.com>
2024-01-02 12:00:00 +0100

    develop dummy event loop

--- chapter-1/dummy-event-loop.ts
+++ chapter-1/dummy-event-loop.ts
@@ -10,4 +10,12 @@ const shouldContinue = () => {
 };
 
 while (shouldContinue()) {
+  // 1) Node looks at pendingTimers and sees if any functions are ready to be called. setTimeout, setInterval
+  // 2) Node looks at pendingOSTasks and pendingOperations and calls relevant callbacks
+  // 3) Pause execution. Continue when...
+  //  - a new pendingOSTask is done
+  //  - a new pendingOperation is done
+  //  - a timer is about to complete
+  // 4) Look at pendingTimers. Call any setImmediate
+  // 5) Handle any 'close' events
 }
