f5a1a4de3f184a52eacfca7e649b1f0b41e11b3b
lapeko <lapeko88@gmail.com>
2024-01-04 23:43:57 +0100

    create simple express server

--- chapter-2/server.ts
+++ chapter-2/server.ts
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Ok"));
+const doWork = (duration: number) => {
+  const start = Date.now();
+  while (Date.now() < start + duration) {}
+}
+
+app.get("/", (req, res) => {
+  doWork(10000);
+  res.send("Ok");
+});
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
