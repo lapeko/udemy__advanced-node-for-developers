e2fe1015a258f5bbd62c3c380301d4fff8c78fc5
lapeko <lapeko88@gmail.com>
2024-01-05 23:40:04 +0100

    init chapter-3 project

---
+++ chapter-3/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Blogster! 123
+      </h1>
+      Write private blogs
+    </div>
+  );
+};
+
+export default Landing;
