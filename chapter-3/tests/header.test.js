0d1e286195ebe445feab1af0befd7b3b587c2d6b
lapeko <lapeko88@gmail.com>
2024-01-06 23:37:36 +0100

    run puppeteer in test

--- chapter-3/tests/header.test.js
+++ chapter-3/tests/header.test.js
@@ -1,5 +1,10 @@
-describe("Test test", () => {
-  it("Check sum of 2 and 2", () => {
-    expect(2 + 2).toBe(4);
-  });
+const puppeteer = require("puppeteer");
+
+it("Check sum of 2 and 2", () => {
+  expect(2 + 2).toBe(4);
+});
+
+it("run a browser", async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
 });
