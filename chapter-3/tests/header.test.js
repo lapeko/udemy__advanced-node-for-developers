147f3534362ec74a93a6e83d5ab32e0ac03ae622
lapeko <lapeko88@gmail.com>
2024-01-07 11:30:16 +0100

    authenticate with cookies for testing

--- chapter-3/tests/header.test.js
+++ chapter-3/tests/header.test.js
@@ -1,17 +1,18 @@
 const puppeteer = require("puppeteer");
-
+const Keygrip = require("keygrip");
+const cookieKey = require("../config/keys").cookieKey;
 
 let browser, page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch(/*{headless: false}*/);
+  browser = await puppeteer.launch({headless: false});
   page = (await browser.pages())[0];
   if (!page) page = await browser.newPage();
   await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  // await browser.close();
 });
 
 it("should have expected header", async () => {
@@ -28,4 +29,17 @@ it("should visit auth page", async () => {
   const url = await page.url();
 
   expect(url).toMatch(/^https:\/\/accounts.google.com\//);
+});
+
+it.only("Bla bla", async () => {
+  const id = "65988b48c12389babc91994e";
+  const sessionObject = {passport: {user: id}};
+  const sessionString = btoa(JSON.stringify(sessionObject));
+  const keygrip = Keygrip([cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+
+  await page.setCookie({name: "session", value: sessionString});
+  await page.setCookie({name: "session.sig", value: sig});
+
+  await page.goto("http://localhost:3000/blogs");
 });
