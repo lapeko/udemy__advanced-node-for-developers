8212333b0003b1ced06c34616f598c63700e8828
lapeko <lapeko88@gmail.com>
2024-01-08 00:25:46 +0100

    form should show validation errors

--- chapter-3/tests/blogs.test.js
+++ chapter-3/tests/blogs.test.js
@@ -3,19 +3,31 @@ const Page = require("./utils/page");
 let page;
 
 describe("Blogs", () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     page = await Page.build({headless: "new"});
   });
 
-  afterEach(async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click("a.btn-floating");
+  });
+
+  afterAll(async () => {
     await page.close();
   });
 
   it("should open blog creation form", async () => {
-    await page.login();
-    await page.click("a.btn-floating");
     const label = await page.getContentBySelector("form label");
 
     expect(label).toEqual("Blog Title");
+  });
+
+  it("should show error message if invalid inputs", async () => {
+    await page.click("form button");
+    const titleError = await page.getContentBySelector(".title .red-text");
+    const contentError = await page.getContentBySelector(".content .red-text");
+
+    expect(titleError).toEqual("You must provide a value");
+    expect(contentError).toEqual("You must provide a value");
   });
 });
